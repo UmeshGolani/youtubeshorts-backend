8f0617bba5692979858e27e2cbab0e18c57769c5
UmeshGolani <ssutg97@gmail.com>
2024-02-25 23:44:31 +0530

    port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 // Define MongoDB schema
 const videoSchema = new mongoose.Schema({
